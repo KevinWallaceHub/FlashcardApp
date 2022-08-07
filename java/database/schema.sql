5812f7776762c9647cda004dfeb047ab562b9aa7
Chris Linscott <chris.linscott00@gmail.com>
2022-08-06 21:03:56 -0400

    flashcard animation

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -34,5 +34,10 @@ CREATE TABLE flashcards(
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'test question','test answer','question stuff things');
+INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'What is the meaning of this','meh','question stuff things');
+INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'but why','because','dogs cats things');
+INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'tester test question','test answer','question stuff things');
+INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'test question','test answer','question stuff things');
+INSERT INTO flashcards(user_id,question_side, answer_side , keywords) VALUES('1', 'test question','test answer','question stuff things');
 
 COMMIT TRANSACTION;
--- vue/src/components/FlashCard.vue
+++ vue/src/components/FlashCard.vue
@@ -5,9 +5,14 @@
       {{flashcard.keywords}}
       {{flashcard.card_id}}
        -->
-      <div 
-       ><p  id="question">{{flashcard.question_side}}</p>
-      <p id="answer">{{flashcard.answer_side}}</p></div>
+      <div class="sideOne">
+          <div class="front">
+              <p  id="question">{{flashcard.question_side}}</p>
+          </div>
+          <div class= "back">
+      <p id="answer">{{flashcard.answer_side}} </p>
+      </div>
+      </div>
 <!-- v-if="!display" v-on:click="display=true"
  v-on:click="display=false" v-show="display"   -->
   </div>
@@ -32,28 +37,61 @@ export default {
 <style>
 div#card  {
     display: flex;
-    border: 5px;
-    border-style:double;
+    /* border: 2px;
+    border-style:solid; */
     margin: 10px;
     text-align: center;
     justify-content: center;
     flex-direction: row;
     width:25%;
+    /* everything between line 48 and 79 is relating to the card flip animation*/
+    perspective: 1000px; 
+    background-color:transparent;
+    font-size: 20pt;
+}
+     div.sideOne{
+        position: relative;
+        transition:transform 1.1s;
+        transform-style: preserve-3d;
+        box-shadow: 2px 4px 8px 0 rgba(0, 0, 0, 0.2);
+        width:100%;
+        
+    } 
+
+     div#card:hover .sideOne{
+        transform: rotateY(180deg)
+    }
+
+    .front, .back{
+        
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+    }
+    .sideOne{
+        background-color:rgb(247, 241, 234);
+        border-style:inset;
+        border-width: 1px;
+        border-radius: 5px;
+            }
     
-  
+    .back{
+    transform: rotateY(180deg);
+    } 
+    
+    p#question {
+    border-width: 0px 0px 2px 0px;
+    border-style:solid;
 }
 
-p#question {
-  
-    border-width: 0px 0px 3px 0px;
-    border-style: solid;
-}
 p#answer{
-    color:white;
+    border-width: 0px 0px 2px 0px;
+    border-style:solid;
+    ;
+    /* color:white; */
 }
-p#answer:hover {
+/* p#answer:hover {
   color:black;
     
-}
+}  */
 
 </style>
