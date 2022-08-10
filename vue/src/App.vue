e6bbdd0a1406561413eb766c20522693e56919c9
Ben Helland <benrhelland@gmail.com>
2022-08-10 11:43:32 -0400

    home page changes

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,34 @@
     <router-view />
   </div>
 </template>
+
+<style>
+
+div.nav {
+    display: flex;
+      align-items: center;
+      justify-content: center;
+      position: sticky;
+      top: 0;
+      cursor: pointer;
+  }
+  body {
+    height: 125vh;
+    background-size: cover;
+    font-family: sans-serif;
+}
+
+main {
+    color: white;
+}
+
+header {
+    background-color: white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 80px;
+}
+
+</style>
