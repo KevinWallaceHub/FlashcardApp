1da919a9d9953878fdd148bb4b7ed3f9596435f1
Ben Helland <benrhelland@gmail.com>
2022-08-10 14:14:28 -0400

    grid works

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="home">
     <div class="main">
-    <h1>Welcome!</h1>
-    <p class="greeting">Expanding your knowledge and studying hard is {website name}'s number one goal!</p>
-    <router-link :to="{ name: 'cards' }"
-      ><button id="yourCards">Your cards</button></router-link
-    >
-    </div>
-    <div class="sidebars">
-      <div class="sidebar"></div>
-      <div class="sidebar"></div>
+      <h1>Welcome!</h1>
+      <p class="greeting">
+        Expanding your knowledge and studying hard is {website name}'s number
+        one goal!
+      </p>
+      <div id="yourCardsDiv">
+        <router-link :to="{ name: 'cards' }"
+          ><button id="yourCards">Your cards</button></router-link
+        >
+      </div>
+      <div class="sidebars">
+        <div class="sidebar1"></div>
+        <div class="sidebar2"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,10 +29,9 @@ export default {
 <style>
 div.home {
   display: grid;
-  grid-template-columns: 1fr, 4fr, 1fr;
-  grid-template-areas: 
-    "sidebar header sidebar"
-    "sidebar main sidebar";
+  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-areas: "sidebar1 main sidebar2";
+  gap: 10px;
 }
 body {
   background-color: #dff0f9;
@@ -36,27 +40,40 @@ body {
 h1 {
   text-align: justify;
 }
-
-div.sidebar {
-  grid-area: sidebar;
+div.sidebars {
+  background-color: #6d9f9c;
+}
+div.sidebar1 {
+  grid-area: sidebar1;
+}
+div.sidebar2 {
+  grid-area: sidebar2;
 }
 div.main {
   grid-area: main;
 }
-
-button#yourCards {
-    width: 25%;
+div#yourCardsDiv {
+  text-align: center;
 }
 
-button {
-    width: 9.7rem;
-    padding: 10px;
-    border-radius: 5px;
-    color: #ffffff;
-    background-color: #e47b64;
-    font-weight: bold;
-    text-align: center;
-    text-transform: uppercase;
-    cursor:pointer;
+button#yourCards {
+  width: 9.7rem;
+  padding: 10px;
+  border-radius: 5px;
+  color: #ffffff;
+  background-color: #ffc7b0;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+button#yourCards {
+  width: 25%;
+  margin-bottom: 30px;
+  margin-top: 10px;
+}
+button#yourCards:hover {
+  background-color: #e47b64;
+  transition: 0.7s;
 }
 </style>
