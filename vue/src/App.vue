556788a9ca053e06ac3ce24c9ca3270bc7201220
Kevin Wallace <Kevinwallace0546@gmail.com>
2022-08-10 10:08:22 -0400

    based style started working on database and forms

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,12 @@
     <router-view />
   </div>
 </template>
+
+<style>
+ div#app{
+   background-image: linear-gradient(to bottom right, #DFF0F9, #215350);
+
+   /* background-image: linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5)),
+                  url("../../media/examples/lizard.png"); */
+ }
+</style>
