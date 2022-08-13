97633a7e64c7fd532dffb26b2f09062aedf0ee63
Chris Linscott <chris.linscott00@gmail.com>
2022-08-13 14:36:52 -0400

    random commit

--- vue/src/components/StudyFlashCard.vue
+++ vue/src/components/StudyFlashCard.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-      <div class="sideOne">
-          <div class="front" v-if="this.switchSide">
-              <span  id="question">{{flashcard.question_side}}</span>
-          </div>
-          <div class="back" v-if="!this.switchSide">
-                <span  id="answer">{{flashcard.answer_side}} </span>
-            </div>
-      </div>
-      <div>
-          <button v-on:click="switchTo()"> Next</button>
+    <div class="sideOne">
+      <div class="card">
+        <p v-if="this.switchSide" id="question"
+          >{{ flashcard.question_side }}
+        </p>
+        <p v-if="!this.switchSide" id="answer"
+          >{{ flashcard.answer_side }}
+        </p>
       </div>
     </div>
     <div>
@@ -17,6 +15,7 @@
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
